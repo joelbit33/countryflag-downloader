fc973d5c99f4cf4ebc37318b52ad2815e07ae380
joelbit33 <j.oskar.nilsson@gmail.com>
2022-11-17 00:20:20 +0100

    update functions

--- flagfinder.py
+++ flagfinder.py
@@ -2,26 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def download_flag(country_name, image_src):
+
+    location_saved = f'downloaded_flags/{country_name}.jpg'
+    with open(location_saved, 'wb') as handler:
+        handler.write(image_src)
+
+
+def find_flag(main_url):
+    """Finds image source link from the main url
+    """
+    country_name = input("Country Name:").lower()
+
+    flag_info_url = f'{main_url}/{country_name}'
+    flag_response = requests.get(flag_info_url)
+    flag_soup = BeautifulSoup(flag_response.content, 'html.parser')
+
+    image = flag_soup.select('div img')
+    image_path = image[0]['src']
+    image_src = requests.get(
+        f'{main_url}{image_path}').content
+    return country_name, image_src
+
+
 main_url = 'https://flagpedia.net'
-country_name = input("Country Name:").lower()
+country_name, image_src = find_flag(main_url)
 
-
-flag_info_url = f'{main_url}/{country_name}'
-flag_response = requests.get(flag_info_url)
-flag_soup = BeautifulSoup(flag_response.content, 'html.parser')
-
-
-image = flag_soup.select('div img')
-image_folder = image[0]['src']
-print(image_folder)
-
-
-###############################
-
-
-img_data = requests.get(
-    f'{main_url}{image_folder}').content
-
-location_saved = f'downloaded_flags/{country_name}.jpg'
-with open(location_saved, 'wb') as handler:
-    handler.write(img_data)
+download_flag(country_name, image_src)
